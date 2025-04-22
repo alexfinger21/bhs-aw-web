ebf0aa3afcb1450cf91a6b5428560fff7b00ae4f
alexfinger21 <cdx9933@gmail.com>
2025-04-22 00:11:20 -0400

    ho dynamic images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,1 +1,8 @@
-<template></template>
+<script>
+    import "@/assets/home.css"
+</script>
+<template>
+    <div class="img-grid">
+        <img class="grid-item" src="@/assets/imgs/Bison-logo-2c-on-white_1.webp"></img> 
+    </div>
+</template>
