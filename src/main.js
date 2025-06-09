0fa75e09b4ef391e41f1e106342e228d55ad80a0
alexfinger21 <cdx9933@gmail.com>
2025-06-08 23:56:42 -0400

    redux

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import "./assets/main.css"
-
 import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./js/router"
+import { provideStoreToApp } from "@reduxjs/vue-redux"
 
-createApp(App)
-    .use(router)
-    .mount('#app')
+const app = createApp(App)
+provideStoreToApp(app)
+app.use(router).mount('#app')
+    
